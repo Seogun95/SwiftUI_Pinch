7d2d844721c6d973aa92711f30ca6f0465a7f23d
SeonJungKim <ksj083895@gmail.com>
2022-03-03 18:28:54 +0500

    DrageGesture

--- Shared/Screen/ContentView.swift
+++ Shared/Screen/ContentView.swift
@@ -12,7 +12,14 @@ struct ContentView: View {
     //MARK: - PROPERTY
     @State private var isAnimating: Bool = false
     @State private var imageScale: CGFloat = 1
+    @State private var imageOffset: CGSize = .zero
     //MARK: - FUNCTION
+    func resetImageState() {
+        withAnimation(.spring()) {
+            imageScale = 1
+            imageOffset = .zero
+        }
+    }
     
     //MARK: - CONTENT
     
@@ -20,26 +27,44 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             ZStack {
-             //MARK: - PAGE IMAGE
-             Image("magazine-front-cover")
+                //MARK: - PAGE IMAGE
+                Image("magazine-front-cover")
                     .resizable()
                     .aspectRatio(contentMode: .fit)
                     .cornerRadius(10)
                     .padding()
                     .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
                     .opacity(isAnimating ? 1 : 0)
+                    .offset(x: imageOffset.width, y: imageOffset.height)
                     .scaleEffect(imageScale)
+                //MARK: - 1. onTapGesture
                     .onTapGesture(count: 2, perform: {
                         if imageScale == 1 {
                             withAnimation(.spring()) {
-                             imageScale = 5
+                                imageScale = 5
                             }
                         } else {
-                            withAnimation(.spring()) {
+                           /* withAnimation(.spring()) {
                                 imageScale = 1
-                            }
+                            }  */
+                            resetImageState()
                         }
                     })
+                //MARK: - 2. DrageGesture
+                    .gesture(
+                        DragGesture()
+                            .onChanged { gesture in
+                                withAnimation(.linear(duration: 1)) {
+                                    imageOffset = gesture.translation
+                                }
+                            }
+                            .onEnded { _ in
+                                if imageScale <= 1 {
+                                    resetImageState()
+                                }
+                            }
+                        
+                    )
             } //: ZStack
             .navigationTitle("Pinch & Zoom")
             .navigationBarTitleDisplayMode(.inline)
