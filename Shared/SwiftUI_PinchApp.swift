5b548b7309bd65e91f41a7ccda4794f4587b9478
SeonJungKim <ksj083895@gmail.com>
2022-03-03 17:34:09 +0500

    Initial Commit

---
+++ Shared/SwiftUI_PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_PinchApp.swift
+//  Shared
+//
+//  Created by Seogun Kim on 2022/03/03.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
